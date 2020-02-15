7d3a12a28415d19be4f1c7894b9365a21d56808c
evboggs302 <evboggs94@gmail.com>
2020-02-14 21:46:20 -0500

    added more vizs

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TableauClass />
+        {/* <TableauClass /> */}
         <br />
         <TableauFunc />
         <br />
--- src/VizzFunc.js
+++ src/VizzFunc.js
@@ -12,14 +12,16 @@ function Test2() {
   var viz, workbook, sheet;
 
   var options = {
-    // height: auto,
-    // width: auto
+    // height: 500,
+    // width: 5000
+    // margin: 20
     // filter_name: filter_value
   };
 
   const initViz2 = () => {
-    const vizUrl =
-      "http://public.tableau.com/views/RegionalSampleWorkbook/Storms";
+    // const vizUrl = `http://public.tableau.com/views/RegionalSampleWorkbook/Storms`;
+    const vizUrl = `https://public.tableau.com/views/WojBombs/WojBombs?:display_count=n&:origin=viz_share_link`;
+    // const vizUrl = `https://public.tableau.com/views/MigrantdeathsanddisappearancesintheMediterranean/MigrantsintheMediterranean?:display_count=n&:origin=viz_share_link`;
     const vizContainer = document.getElementById("vizContainer");
     viz = new window.tableau.Viz(vizContainer, vizUrl, options);
   };
